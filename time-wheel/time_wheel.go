f8eb59e8b46905b1dd5e4bce7f454e8902771ac4
cyh <cyh@cyhdeMacBook-Pro.local>
2019-01-06 16:37:48 +0800

    finish

--- time-wheel/time_wheel.go
+++ time-wheel/time_wheel.go
@@ -22,6 +22,7 @@ type TmObj struct {
 	mode int
 	task TmTask
 	leftRound int
+	cntRound int
 	slotNo int
 }
 
@@ -59,15 +60,17 @@ func (l *TmObjList) del(item *TmObjList) {
 	prev := item.prev
 	next := item.next
 	prev.next = next
-	next.prev = prev
+	if next != nil {
+		next.prev = prev
+	}
 }
 
 type TimerWheel interface {
-	addTimer(task TmTask, waitTime int, mode int)
+	AddTimer(task TmTask, waitTime int, mode int)
 	Stop()
 }
 
-func (td *TmDevice) addTimer(task TmTask, waitTime int, mode int) {
+func (td *TmDevice) AddTimer(task TmTask, waitTime int, mode int) {
 	round := waitTime / (td.interval * slotNum)
 	roundLeft := waitTime % (td.interval * slotNum)
 	slotPos := roundLeft + td.pivot
@@ -87,11 +90,32 @@ func (td *TmDevice) addTimer(task TmTask, waitTime int, mode int) {
 }
 
 func (td *TmDevice) addTask(obj *TmObj) {
-
+	l := td.slots[obj.slotNo]
+	l.add(obj)
 }
 
 func (td *TmDevice) scanTask() {
+	curTaskHead := td.slots[td.pivot]
+	td.pivot++
+	if td.pivot == slotNum {
+		td.pivot = 0
+	}
 
+	for item:=curTaskHead.next; item!=nil; item=item.next {
+		task := item.tmObj
+		if task.leftRound > task.cntRound {
+			task.cntRound++
+		} else {
+			if task.mode == OneShot {
+				//only run once, delete
+				curTaskHead.del(item)
+			} else {
+				//reset count
+				task.cntRound = 0
+			}
+			go task.task.Run()
+		}
+	}
 }
 
 func (td *TmDevice) Stop() {
--- time-wheel/time_wheel_test.go
+++ time-wheel/time_wheel_test.go
@@ -3,6 +3,7 @@ package time_wheel
 import (
 	"testing"
 	"fmt"
+	"time"
 )
 
 func TestAdd(t *testing.T) {
@@ -20,9 +21,42 @@ func TestAdd(t *testing.T) {
 		if i.tmObj.mode %2 == 0 {
 			head.del(i)
 		}
+
+		if i.tmObj.mode == 9 {
+			head.del(i)
+		}
 	}
 
 	for i:=head.next;i!=nil;i=i.next {
 		fmt.Println(i.tmObj)
 	}
+}
+
+type T1 struct {
+	cnt int
+}
+
+func (t *T1) Run() {
+	t.cnt++
+	fmt.Println("this is T1", t.cnt, time.Now().Format("2006-01-02 15:04:05"))
+}
+
+type T2 struct {
+
+}
+
+func (t *T2) Run() {
+	fmt.Println("this is T2")
+}
+
+func TestInitTimer(t *testing.T) {
+	tmDev := InitTimer(10)
+
+	t1 := &T1{}
+	t2 := &T2{}
+
+	tmDev.AddTimer(t1, 1000, Continual)
+	tmDev.AddTimer(t2, 500, OneShot)
+
+	time.Sleep(20*time.Second)
 }
